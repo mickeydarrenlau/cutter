5f26c18dc630dc4bc5a3c19238ad14e64fc92b80
Henrique <henriquechigumane@outlook.com>
2020-10-05 13:13:23 +0200

    reactoring the folder structure

--- v/redirect.js
+++ v/redirect.js
@@ -1,34 +1,35 @@
 endpoint = "https://jsonbox.io/box_e095644c681d08a9e207";
 
 function getUrl(ed) {
-  fetch(ed, {
-    method: "GET",
-  })
-    .then((res) => res.json())
-    .then((res) => {console.log(res) 
-       redirecter(res)
-    }
-    )
+    fetch(ed, {
+        method: "GET",
+    })
+        .then((res) => res.json())
+        .then((res) => {
+            console.log(res)
+            redirecter(res)
+        }
+        )
 }
-function getPahtId (){
+function getPahtId() {
     let path = window.location.hash;
     console.log(path);
-    if(path=="/"){
+    if (path == "/") {
         return "";
         console.log("none")
     }
     //return path.pathname.split("/")[3];
-   return path.split("#")[1]
+    return path.split("#")[1]
 }
-const pathId= getPahtId();
-getUrl(endpoint+"/?q=id:"+pathId);
+const pathId = getPahtId();
+getUrl(endpoint + "/?q=id:" + pathId);
 
 function redirecter(longURL) {
-    if (longURL.length>=1) {
-        window.location.href=longURL[0].url;
-     }
-     else{
-         window.location.href="https://misterpaps.me/404"
-     }
-     
+    if (longURL.length >= 1) {
+        window.location.href = longURL[0].url;
+    }
+    else {
+        window.location.href = "https://misterpaps.me/404"
+    }
+
 }
